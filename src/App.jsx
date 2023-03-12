cbae0fae61776e36d379b4adb5da9749a670c5c7
Efosa Lawal <fodrive@Efosas-MP.local>
2023-03-12 23:07:32 +0000

    Cleaned out default code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
+import React from "react";
 import "./App.scss";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return; /*(
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );*/
+  return (
+    <>
+      <h1>And we begin...</h1>
+    </>
+  );
 }
 
 export default App;
